1eebd8e9fa78e1da01926cb90f04af1625fb0868
ThomasTel <thomastel94@hotmail.fr>
2015-12-25 02:49:02 +0100

    color Codeforces countries only

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def random_color():
     r = lambda: random.randint(0,255)
     return '#%02X%02X%02X' % (r(),r(),r())
 
-    
 d = {}
 fr = io.open('ratings_treated_code.csv', 'r', encoding='utf8')
 for s in fr.read().splitlines():
@@ -23,10 +22,9 @@ fr.close()
 tree = etree.parse('Codeforces_rating.svg')
 root = tree.getroot()[0]
 for child in root:
-   #if 'path' in child.tag:
-      #child.attrib['fill'] = random_color()'''
-   color = random_color()
-   for path in child.iter('{http://www.w3.org/2000/svg}path'):
-      path.attrib['fill'] = color
+   if 'id' in child.attrib and child.attrib['id'] in d:
+      color = random_color()
+      for path in child.iter('{http://www.w3.org/2000/svg}path'):
+         path.attrib['fill'] = color
 
 tree.write('Codeforces_rating_treated.svg')
